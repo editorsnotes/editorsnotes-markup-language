9350bedec55302b604d6bcfe4e464b276d30b992
Patrick Golden <ptgolden0@gmail.com>
2015-07-17 12:53:14 -0400

    Support using @@item syntax for link hrefs

---
+++ src/md_items_href.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const TYPES = require('./types')
+    , referenceRegex = /^@@([ntd])(\d+)$/
+
+
+function transformHref(token, makeURL) {
+  var hrefIndex
+    , relIndex
+    , match
+
+  for (var i = 0; i < token.attrs.length; i++) {
+    if (token.attrs[i][0] === 'href') hrefIndex = i;
+    if (token.attrs[i][0] === 'rel') relIndex = i;
+  }
+
+  if (hrefIndex === undefined) return;
+
+  match = token.attrs[hrefIndex][1].match(referenceRegex);
+
+  if (match) {
+    let itemType = TYPES[match[1]]
+      , itemID = match[2]
+
+    token.attrs[hrefIndex][1] = makeURL(itemType, itemID);
+
+    if (relIndex !== undefined) {
+      let oldRel = token.attrs[relIndex][1];
+      token.attrs[relIndex][1] = oldRel + ' ' + itemType;
+    } else {
+      token.attrs.push([ 'rel', itemType ])
+    }
+  }
+}
+
+function createRule(md, projectBaseURL) {
+  var makeURL = require('./get_item_url').bind(null, projectBaseURL)
+
+  return function enItemsHref(state) {
+    state.tokens.forEach(function (blockToken) {
+      if (blockToken.type === 'inline' && blockToken.children) {
+        blockToken.children.forEach(function (token) {
+          var type = token.type;
+          if (type === 'link_open') {
+            transformHref(token, makeURL);
+          }
+        });
+      }
+    });
+  }
+}
+
+module.exports = function (md, opts) {
+  opts = opts || {};
+
+  md.core.ruler.after(
+    'inline',
+    'href_en_ref',
+    createRule(md, opts.projectBaseURL)
+  )
+}
--- src/test/all.js
+++ src/test/all.js
@@ -24,4 +24,30 @@ test('Inline item links', function (t) {
     parser.render('mentioning @@n456!').trim(),
     '<p>mentioning <a class="en-item en-item-note" rel="note" href="/projects/emma/notes/456/">note 456</a>!</p>'
   )
-})
+});
+
+test('Items as hrefs', function (t) {
+  var itemHrefPlugin = require('../md_items_href')
+    , parser
+
+  parser = require('markdown-it')().use(itemHrefPlugin, {
+    projectBaseURL: '/projects/emma/',
+  });
+
+  t.plan(3);
+
+  t.equal(
+    parser.render('This is a link to [a topic](@@t500).').trim(),
+    '<p>This is a link to <a href="/projects/emma/topics/500/" rel="topic">a topic</a>.</p>'
+  )
+
+  t.equal(
+    parser.render('This is a link to [a note](@@n365).').trim(),
+    '<p>This is a link to <a href="/projects/emma/notes/365/" rel="note">a note</a>.</p>'
+  )
+
+  t.equal(
+    parser.render('This is a link to [a document](@@d7).').trim(),
+    '<p>This is a link to <a href="/projects/emma/documents/7/" rel="document">a document</a>.</p>'
+  )
+});
