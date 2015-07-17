842cae76512fbfddb678e4ba43ac49de46629f6e
Patrick Golden <ptgolden0@gmail.com>
2015-07-17 15:25:55 -0400

    Add whole parser to index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 "use strict"
+
+module.exports = function (opts) {
+  var inlinePlugin = require('./md_inline_items')
+    , hrefPlugin = require('./md_items_href')
+
+  opts = opts || {};
+
+  if (!opts.projectBaseURL) {
+    throw Error("Must pass projectBaseURL");
+  }
+
+  if (!opts.resolveItemText) {
+    throw Error("Must pass resolveItemText");
+  }
+
+  return require('markdown-it')()
+    .use(inlinePlugin, {
+      projectBaseURL: opts.projectBaseURL,
+      resolveItemText: opts.resolveItemText
+    })
+    .use(hrefPlugin, {
+      projectBaseURL: opts.projectBaseURL
+    })
+}
