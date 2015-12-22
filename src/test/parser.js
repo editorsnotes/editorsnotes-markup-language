ae5b0b57ba7b181b512cd828c3cf5a0b1ee86704
Patrick Golden <ptgolden0@gmail.com>
2015-12-22 16:08:30 -0500

    Add a test for blockquote citation attribution in a document block

---
+++ src/test/parser.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var test = require('tape')
+
+test('Citations in document blocks', function (t) {
+  var parser = require('../')({
+    projectBaseURL: '/',
+    makeInlineCitation: cites => ({
+      delimiter: null,
+      citations: cites.map(d => 'INLINE ' + d.id + ', ' + d.locator),
+    }),
+    makeBibliographyEntry: d => 'BIBLIOGRAPHY ' + d.id,
+    resolveItemText: d => null
+  })
+
+  var markdown = `
+::: document @@d100
+
+> This is a
+> quote.
+>
+> [p. 12]
+
+:::
+`.trim()
+
+  var expectedHTML = (
+    '<section class="ENDocumentBlock"><div>' +
+    '<a href="/documents/100/" class="ENDocumentBlock--Citation">BIBLIOGRAPHY 100</a>' +
+    '</div>' +
+    '<blockquote>\n' +
+      '<p>This is a\nquote.</p>\n' +
+      '<footer><cite>' +
+      '<a href="/documents/100/" class="ENInlineReference ENInlineReference-document">' +
+      'INLINE 100, p. 12' +
+      '</a></cite></footer>\n' +
+    '</blockquote>\n' +
+    '</section>'
+  )
+
+  t.plan(1);
+  t.equal(parser.render(markdown, {}), expectedHTML);
+});
